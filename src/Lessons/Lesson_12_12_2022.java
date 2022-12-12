064631d08eb42ea08b03eb972097a33f4fec1702
serg <szabolotnyi777@gmail.com>
2022-12-12 22:16:17 +0100

    version 12.12.2022

---
+++ src/Lessons/Lesson_12_12_2022.java
@@ -0,0 +1,16 @@
+package Lessons;
+
+public class Lesson_12_12_2022 {
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 15;
+    int results = sum(a,b);
+        System.out.println(results);
+    }
+    public static int sum (int x, int y){
+    int sum = x * y;
+    return sum;
+    }
+
+    public static void printText 
+}
