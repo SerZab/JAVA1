ee9b084c2feadb4b673b85232834c926c5ee65be
serg <szabolotnyi777@gmail.com>
2022-12-13 15:34:06 +0100

    version 13.12.2022

--- src/Lessons/Lesson_12_12_2022.java
+++ src/Lessons/Lesson_12_12_2022.java
@@ -2,15 +2,30 @@ package Lessons;
 
 public class Lesson_12_12_2022 {
     public static void main(String[] args) {
-    int a = 10;
-    int b = 15;
-    int results = sum(a,b);
-        System.out.println(results);
-    }
-    public static int sum (int x, int y){
-    int sum = x * y;
-    return sum;
+
+        int a1 = 10;
+        int b1 = 15;
+
+        int a2 = 100;
+        int b2 = 150;
+
+        int result1 = sum(a1,b1);
+
+        printText(1, result1);
+
+        printText(2,sum(a2,b2));
+
     }
 
-    public static void printText 
+    //=================================
+
+    public static int sum (int x, int y){
+        int sum = x + y;
+        return sum;
+    }
+
+    //=================================
+    public static void printText (int i, int text){
+        System.out.println("Result sum " + i +  " = " + text);
+    }
 }
